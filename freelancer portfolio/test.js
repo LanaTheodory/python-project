402ff78d5f06a3ceb9ddea0312fa80d5604b23a7
abdullahqasem2 <abdullahzq97@gmail.com>
2021-06-03 01:19:04 +0300

    abd

---
+++ freelancer portfolio/test.js
@@ -0,0 +1,22 @@
+let chatBoxPerview = document.querySelector(".perview-box-msg");
+let msgBoxSend = document.querySelector("#msgBoxSend");
+let msgBtnSend = document.querySelector("#sendMsg");
+
+function sendMessage() {
+  const msgValue = msgBoxSend.value;
+  if (msgValue.trim().length == 0) return false;
+
+  const newMsg = document.createElement("div");
+  newMsg.className = "mrm-msg";
+  newMsg.innerText = msgValue;
+  chatBoxPerview.append(newMsg);
+  msgBoxSend.value = "";
+  msgBoxSend.focus();
+}
+
+msgBtnSend.onclick = sendMessage;
+msgBoxSend.onkeyup = (e) => {
+  if (e.key === "Enter") {
+    sendMessage();
+  }
+};
